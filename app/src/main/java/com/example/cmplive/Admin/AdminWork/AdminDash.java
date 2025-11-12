0a618645a71d1478cf92e3f6d9d82659fe217d44
mayank4singh <mailatmausam@gmail.com>
2025-11-12 14:38:48 +0530

    Initial Commit

--- app/src/main/java/com/example/cmplive/Admin/AdminWork/AdminDash.java
+++ app/src/main/java/com/example/cmplive/Admin/AdminWork/AdminDash.java
@@ -31,6 +31,7 @@ public class AdminDash extends AppCompatActivity implements NavigationView.OnNav
  BottomNavigationView bottomNavigationView;
  NavigationView navigationView;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
